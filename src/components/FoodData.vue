c0d43173bb06923ba6bd301ab6d5cca2eb9be9b0
Apregi Prata Yuda <apregipratayda@gmail.com>
2025-07-16 12:17:06 +0700

    modifed

--- src/components/FoodData.vue
+++ src/components/FoodData.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 const emit = defineEmits(['add-food','delete-food', 'update-food'])
 const titleTable = ref('List Product Alfamart');
 
@@ -36,13 +36,23 @@ function saveUpdate() {
   editedItem.value = {}
 }
 
+const keyword = ref('');
+// Computed untuk filter berdasarkan keyword
+const filteredFoods = computed(() => {
+  if (!keyword.value) return propsFood.foods
+
+  return propsFood.foods.filter(food =>
+    food.namaFood.toLowerCase().includes(keyword.value.toLowerCase()) ||
+    food.kategoriFood.toLowerCase().includes(keyword.value.toLowerCase())
+  )
+})
+
+
 </script>
 
 <template>
      <div class="container">
         <h4>{{ titleTable }}</h4>
-
-
           <div v-if="isEditing" class="mb-3 p-3 border rounded">
             <h5>Edit Produk</h5>
             <input v-model="editedItem.namaFood" class="form-control mb-2" placeholder="Nama Produk" />
@@ -54,7 +64,25 @@ function saveUpdate() {
             </div>
 
 
-    <button class="btn btn-primary btn-sm" @click="addData">Add Data</button>
+     <div class="row mb-0">
+     <div class="col">
+    <div class="d-flex justify-content-between align-items-center">
+ 
+      <!-- button add -->
+      <button class="btn btn-primary btn-sm" @click="addData">Add Data</button>
+
+       <!-- component Cari -->
+       <input
+        v-model="keyword"
+        type="text"
+        placeholder="Cari produk..."
+        class="form-control col-md-4"
+       />
+    </div>
+  </div>
+</div>
+
+
     <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
       <table class="table table-bordered">
         <thead>
@@ -68,14 +96,22 @@ function saveUpdate() {
           </tr>
         </thead>
         <tbody>
-             <tr v-for="food in propsFood.foods" :key="food.idFood">
+
+        <tr v-if="filteredFoods.length === 0">
+      <td colspan="6" class="text-center text-muted">
+        🔍 Tidak ditemukan data yang cocok dengan pencarian.
+      </td>
+    </tr>
+
+
+             <tr v-for="food in filteredFoods" :key="food.idFood">
                  <td>{{ food.idFood }}</td>
                  <td>{{ food.namaFood }}</td>
                  <td>{{ food.hargaFood }}</td>
                  <td>{{ food.stockFood }}</td>
                  <td>{{ food.kategoriFood }}</td>
                  <td>
-                     <button class="btn btn-warning btn-sm me-2" @click="startUpdate(food)">Update</button>
+                     <button class="btn btn-warning btn-sm mr-2" @click="startUpdate(food)">Update</button>
                     <button class="btn btn-danger btn-sm" @click="deleteItem(food.idFood)">Delete</button>
                  </td>
 
